cc109f50a61e46e362ed2bb151a4cc3ccb864aac
fahrurrozzi <izzor.master@gmail.com>
2017-12-23 06:38:23 +0700

    CRUD capability

--- app/controllers/users.server.controller.js
+++ app/controllers/users.server.controller.js
@@ -30,10 +30,10 @@ exports.userByID = function(req, res, next, id) {
 	User.findOne({
 		_id: id
 	}, (err, user) => {
-	if (err) {
-		return next(err);
-	} else {
-		req.user = user;
+		if (err) {
+			return next(err);
+		} else {
+			req.user = user;
 	next();
 	}
 });
@@ -43,10 +43,20 @@ exports.update = function(req, res, next) {
 	User.findByIdAndUpdate(req.user.id, req.body, {
 		'new': true
 	}, (err, user) => {
-	if (err) {
-		return next(err);
-	} else {
-		res.status(200).json(user);
+		if (err) {
+			return next(err);
+		} else {
+			res.status(200).json(user);
 	}
 });
+};
+
+exports.delete = function(req, res, next) {
+	req.user.remove(err => {
+		if (err) {
+			return next(err);
+		} else {
+			res.status(200).json(req.user);
+		}
+	})
 };
